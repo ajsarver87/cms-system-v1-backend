0126ddaefc412ac4171942e4a251d91eeae4752f
alexandersarver <ajsarver87@gmail.com>
2025-10-26 21:14:25 -0500

    add startup code to create database tables and modify user model

--- app/models.py
+++ app/models.py
@@ -6,14 +6,14 @@ from typing import Optional
 
 class User(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    username: str
+    username: str = Field(unique=True, index=True, max_length=50, nullable=False)
     hashed_password: str
-    first_name: str
-    last_name: str
-    email: str
-    is_active: bool
-    is_admin: bool
-    is_superuser: bool
+    first_name: str = Field(max_length=50, nullable=False)
+    last_name: str = Field(max_length=50, nullable=False)
+    email: str = Field(unique=True, index=True)
+    is_active: bool = Field(default=True)
+    is_admin: bool = Field(default=False)
+    is_superuser: bool = Field(default=False)
     created_at: Optional[datetime] = Field(
         default=None,
         sa_column=Column(
